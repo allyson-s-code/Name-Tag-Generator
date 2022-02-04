fa80c0ffe47be108046e97e191600049edafa691
allyson_s <allysonsmithstudio@gmail.com>
2022-02-04 11:34:04 -0500

    Added user input fuctionality and lifecycle methods to save in browser

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import NameTag from "./NameTag.js";
+import App from "./App.js";
 
 import "./styles.css";
-
-const App = () => (
-  <div className="App">
-    <h1>Name Tag Generator</h1>
-    <NameTag name="Tyson" />
-    <NameTag name="Max" />
-    <NameTag name="Allyson" />
-    <NameTag name="Jayden" />
-    <NameTag name="Marcus" />
-  </div>
-);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
